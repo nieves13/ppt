35406301a3d7fe441ff8bffe98d70b2c5699d322
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-02-03 11:53:51 +0100

    Cambio de lagarto por torito y de spok por sepia

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -6,7 +6,7 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"PIEDRA", "SPOCK", "PAPEL", "LAGARTO", "TIJERAS"};
+    private static final String[] validMoves = {"PIEDRA", "SEPIA", "PAPEL", "TORITO", "TIJERAS"};
     private static final String[] validCommands = {"SALIR", "HELP"};
 
     private Random rnd;
